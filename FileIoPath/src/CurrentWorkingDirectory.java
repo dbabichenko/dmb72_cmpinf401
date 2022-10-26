34081cb13ab205391812f20c0bf3ec0a1b67c91f
dbabichenko <dmb72@pitt.edu>
2022-10-26 09:10:46 -0400

    File path example
    Absolute path vs. relative path.

---
+++ FileIoPath/src/CurrentWorkingDirectory.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Paths;
+
+public class CurrentWorkingDirectory {
+
+    public static void main(String[] args) {
+
+        printCurrentWorkingDirectory1();
+        printCurrentWorkingDirectory2();
+        printCurrentWorkingDirectory3();
+        printCurrentWorkingDirectory4();
+
+    }
+
+    // System Property
+    private static void printCurrentWorkingDirectory1() {
+        String userDirectory = System.getProperty("user.dir");
+        System.out.println(userDirectory);
+    }
+
+    // Path, Java 7
+    private static void printCurrentWorkingDirectory2() {
+        String userDirectory = Paths.get("")
+                .toAbsolutePath()
+                .toString();
+        System.out.println(userDirectory);
+    }
+
+    // File("")
+    private static void printCurrentWorkingDirectory3() {
+        String userDirectory = new File("").getAbsolutePath();
+        System.out.println(userDirectory);
+    }
+
+    // FileSystems
+    private static void printCurrentWorkingDirectory4() {
+        String userDirectory = FileSystems.getDefault()
+                .getPath("")
+                .toAbsolutePath()
+                .toString();
+        System.out.println(userDirectory);
+    }
+
+}
